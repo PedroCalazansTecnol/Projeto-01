cff87a339a5e6f709f3b0c1a1d1a06f8ad609d0c
PedroCalazansTecnol <pedro.calazans@sistemastecnol.com.br>
2025-06-17 14:00:32 -0300

    concluindo rotas da API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
+use Illuminate\Cache\Events\RetrievingKey;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -18,25 +22,89 @@ class UserController extends Controller
    
     public function store(Request $request)
     {
-        
+
+        try{
+        $nome = $request->input('name');
+        $email = $request->input('email');
+        $senha = $request->input('senha');
+
+        User::create([
+            'name' => $nome,
+            'email'=>$email,
+            'password'=>bcrypt($senha),
+        ]);
+
+        $user = User::where('name',$nome)->first();
+
+
+        return response ()->json($user);
+
+        }
+        catch(Exception $e){
+             Log::error($e); 
+            return response()->json([
+            'V1' => $request->input('name'),
+            'V2'=>$request->input('email'),
+            'message' => 'Erro interno no servidor',
+            'erro' => $e->getMessage()
+        ], 500);
+
+        }
         
     }
 
   
-    public function show(User $user)
+    public function show($id)
     {
-        
+      
+        return response ()-> json(User::find($id));
     }
 
     
-    public function update(Request $request, User $user)
+    public function update(Request $request,$id)
     {
         
+
+       try {
+        //VINCULA O ID AO USUARIO NO BANCO
+        $user = User::find($id);
+        // TESTA PARA VER SE O USUARIO EXISTE
+        if (!$user) {
+            return response()->json(['message' => 'Item não encontrado'], 404);
+        }
+        //VALIDA O JSON
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email'
+        ]);
+
+        //VINCULA OS DADOS QUE CHEGARAM VIA JSON DO FORMULARIO E VINCULA ELES AO OBJETO DO BANCO
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->save();
+
+        return response()->json($user);
+
+    } catch (\Throwable $e) {
+        Log::error($e); 
+        return response()->json([
+            'V1' => $request->input('name'),
+            'V2'=>$request->input('email'),
+            'message' => 'Erro interno no servidor',
+            'erro' => $e->getMessage()
+        ], 500);
+    }
+    
+
     }
 
    
-    public function destroy(User $user)
+    public function destroy($id)
     {
-        
+        $user = User::find($id);
+         $resp =  response()->json(User::find($id));
+        $user->delete();
+
+       return $resp;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,11 @@ use App\Http\Controllers\UseController;
 
 //ROTA PARA API CONSEGUIR TODOS O USUARIO CADASTRADOS DENTRO DO BANCO
 Route::get('/users',[UserController::class,'index']);
-Route::get('');
+//RECEBE UM ID E RETORNA O JASON APENAS COM O OBJETO A QUAL ELE ESTÁ VINCULADO
+Route::get('/consulta/{id}',[UserController::class,'show']);
+//RECEBE UM ID E EXCLUI O OBJETO NO BANCO RETORNANDO UM JSON COM OS DADOS DELE
+Route::delete('excluir/{id}',[UserController::class,'destroy']);
+//RECEBE UM JSON E UM ID, CONFERE E FAZ A ATUALIZAÇÃO DO OBJETO COM BASE NOS DADOS RECEBIDO
+Route::put('/att/{id}',[UserController::class,'update']); 
+//METODO PARA CRIAÇÃO DE UM OBJETO NO BANCO
+Route::post('/cadastro',[UserController::class,'store']);
