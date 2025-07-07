7dbc14ebf726104ce3f45be235cc1ddf968f4ae9
PedroCalazansTecnol <pedro.calazans@sistemastecnol.com.br>
2025-07-07 13:44:20 -0300

    adicionando middlware de autenticação nas rotas

--- routes/web.php
+++ routes/web.php
@@ -15,31 +15,31 @@ Route::get('/', [UseController::class, 'index'])-> name('index');
 
 
 //VISUALIZAÇÃO DE DADOS DO BANCO
-Route::get('/visualizar',[UseController::class,'lista'])->name('dados.banco');
+Route::get('/visualizar',[UseController::class,'lista'])->name('dados.banco')->middleware('auth');
 
 //PAGINA DE SUCESSO PARA QUALQUER PROCESSO REALIZADO
-Route::get('form/atualizar/conclusao',[UseController::class,'visu'])->name('msg');
+Route::get('form/atualizar/conclusao',[UseController::class,'visu'])->name('msg')->middleware('auth');
 
 //DIRECIONAMENTO PARA PAGINA DE EXCLUSÃO
-Route::get('form/exclusao',[UseController::class,'exc'])->name('excluir');
+Route::get('form/exclusao',[UseController::class,'exc'])->name('excluir')->middleware('auth');
 
 //EXECUÇÃO DA EXCLUSÃO DE UM USUARIO VIA ID
-Route::get('/form/exclusao/delete',[UseController::class,'delete'])->name('delete');
+Route::get('/form/exclusao/delete',[UseController::class,'delete'])->name('delete')->middleware('auth');
 
 //DIRECIONAMENTO PARA PAGINA DE REGISTRO DE USUSARIO
-Route::get('/form/cadastro',[UseController::class,'cad'])->name('cad.user');
+Route::get('/form/cadastro',[UseController::class,'cad'])->name('cad.user')->middleware('auth');
 
 //DIRECIONAMENTO PARA A PAGINA DE ATUALIZAÇÃO DE DADOS
-Route::get('form/atualizar/',[UseController::class,'att'])->name('att.dados');
+Route::get('form/atualizar/',[UseController::class,'att'])->name('att.dados')->middleware('auth');
 
 //ROTA PARA ATUALIZAÇÃO DE DADOS
-Route::get('/atualizar',[UseController::class,'confirm'])->name('confirm.att');
+Route::get('/atualizar',[UseController::class,'confirm'])->name('confirm.att')->middleware('auth');
 
 //ROTA DE VERIFICAÇÃO DE UM OBJETO DENTRO DO BANCO DE DADOS
-Route::get('verificar',[UseController::class,'verificar'])->name('verificar');
+Route::get('verificar',[UseController::class,'verificar'])->name('verificar')->middleware('auth');
 // ROTA Q VERIFICA UM OBJETO DENTRO DO BANCO DE DADOS
 
-Route::get('verificar/resultado',[UseController::class,'check'])->name('verificar.check');
+Route::get('verificar/resultado',[UseController::class,'check'])->name('verificar.check')->middleware('auth');
 
 //ROTA DE TESTE PARA RETORNAR UM JSON
 
@@ -49,9 +49,9 @@ Route::get('verificar/resultado',[UseController::class,'check'])->name('verifica
 
 //ROTA DE AUTENTICAÇÃO
 
-Route::POST('/autenticar',[UseController::class,'logar'])->name('autenticar');
+Route::POST('/autenticar',[UseController::class,'logar'])->name('autenticar')->middleware('auth');
 
-Route::get('/CRUD',[UseController::class,'crud'])->middleware('auth')->name('home');
+Route::get('/CRUD',[UseController::class,'crud'])->middleware('auth')->name('home')->middleware('auth');
 
 Route::get('/login',function(){
      $msg ="dados incorretos";
